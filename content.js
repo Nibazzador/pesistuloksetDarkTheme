dae974ae554b918e668aae6807702230e9af228a
NiilaVest <88927707+Nibazzador@users.noreply.github.com>
2023-07-29 16:45:56 +0300

    Added popup where user can choose their own colors to use

--- content.js
+++ content.js
@@ -1,45 +1,55 @@
-const BGcolor = "#222";
-const color = "#DDD";
+let bgColor = "";
+let textColor = "";
+chrome.storage.local.get(["bgColor"]).then((result) => {
+  bgColor = result.bgColor;
+  chrome.storage.local.get(["textColor"]).then((result) => {
+    textColor = result.textColor;
 
-document.body.style.backgroundColor = BGcolor;
+    document.body.style.backgroundColor = bgColor;
 
-const changeColors = () => {
-  const changes = document.querySelectorAll(
-    "#app, .organization-selection, .bg-white, .slidebtn, .slidearea, .organizer-area, .btn, .card, .bg-light, th, tr, td"
-  );
-  changes.forEach((change) => {
-    change.style.backgroundColor = BGcolor;
-    change.classList.remove("bg-white");
-    change.classList.remove("bg-light");
-  });
-};
+    const changeBgColors = () => {
+      const changes = document.querySelectorAll(
+        "#app, .organization-selection, .bg-white, .slidebtn, .slidearea, .organizer-area, .btn, .card, .bg-light, th, tr, td"
+      );
+      changes.forEach((change) => {
+        change.style.backgroundColor = bgColor;
+        change.classList.remove("bg-white");
+        change.classList.remove("bg-light");
+      });
+    };
 
-const linkColor = () => {
-  const links = document.querySelectorAll("a");
-  links.forEach((link) => {
-    link.style.color = color;
-  });
-};
+    const changeTextColor = () => {
+      const links = document.querySelectorAll("a, button span, .slidebtn p");
+      links.forEach((link) => {
+        link.style.color = textColor;
+      });
+      const selectedDate = document.querySelector(".selected-date");
+      if (selectedDate) {
+        selectedDate.style.color = bgColor;
+      }
+    };
 
-const headerFix = () => {
-  const statTableHeaders = document.querySelectorAll(
-    ".sticky-header-cloned-wrapper"
-  );
-  const pageNavbar = document.querySelector(".navbar-item.secondary");
+    const headerFix = () => {
+      const statTableHeaders = document.querySelectorAll(
+        ".sticky-header-cloned-wrapper"
+      );
+      const pageNavbar = document.querySelector(".navbar-item.secondary");
 
-  if (statTableHeaders) {
-    statTableHeaders.forEach((statTableHeader) => {
-      statTableHeader.style.top = `${pageNavbar.offsetHeight}px`;
+      if (statTableHeaders) {
+        statTableHeaders.forEach((statTableHeader) => {
+          statTableHeader.style.top = `${pageNavbar.offsetHeight}px`;
+        });
+      }
+    };
+
+    changeBgColors();
+    changeTextColor();
+    headerFix();
+
+    document.addEventListener("scroll", () => {
+      changeBgColors();
+      changeTextColor();
+      headerFix();
     });
-  }
-};
-
-changeColors();
-linkColor();
-headerFix();
-
-document.addEventListener("scroll", () => {
-  changeColors();
-  linkColor();
-  headerFix();
+  });
 });
---
+++ popup.js
@@ -0,0 +1,28 @@
+const colorForm = document.getElementById("colorForm");
+const bgInput = document.getElementById("bgColor");
+const textInput = document.getElementById("textColor");
+
+let bgColor = "";
+let textColor = "";
+
+chrome.storage.local.get(["bgColor"]).then((result) => {
+  bgColor = result.bgColor;
+  chrome.storage.local.get(["textColor"]).then((result) => {
+    textColor = result.textColor;
+
+    bgInput.value = bgColor;
+    textInput.value = textColor;
+    document.body.style.backgroundColor = bgColor;
+    document.body.style.color = textColor;
+  });
+});
+
+colorForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const bgValue = bgInput.value.trim();
+  const textValue = textInput.value.trim();
+  document.body.style.backgroundColor = bgValue;
+  document.body.style.color = textValue;
+  chrome.storage.local.set({ bgColor: bgValue });
+  chrome.storage.local.set({ textColor: textValue });
+});
