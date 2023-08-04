7fdff656d2ac5798082cb0689c7320c11014fa57
NiilaVest <88927707+Nibazzador@users.noreply.github.com>
2023-08-04 19:49:26 +0300

    changed from js to css

--- content.js
+++ content.js
@@ -1,63 +1,40 @@
 let bgColor = "";
 let textColor = "";
 chrome.storage.local.get(["bgColor"]).then((result) => {
-  bgColor = result.bgColor;
-  if (bgColor === undefined) {
-    bgColor = "#123";
-  }
-  chrome.storage.local.get(["textColor"]).then((result) => {
-    textColor = result.textColor;
-    if (textColor === undefined) {
-      textColor = "#aaa";
-    }
+  document.documentElement.style.setProperty(
+    "--backgroundColor",
+    result.bgColor
+  );
+});
+chrome.storage.local.get(["textColor"]).then((result) => {
+  document.documentElement.style.setProperty("--color", result.textColor);
+});
 
-    document.body.style.backgroundColor = bgColor;
-
-    const changeBgColors = () => {
-      const changes = document.querySelectorAll(
-        "#app, .organization-selection, .bg-white, .slidebtn, .slidearea, .organizer-area, .btn, .card, .bg-light, th, td"
-      );
-      changes.forEach((change) => {
-        change.style.backgroundColor = bgColor;
-        change.classList.remove("bg-white");
-        change.classList.remove("bg-light");
-      });
-    };
-
-    const changeTextColor = () => {
-      const texts = document.querySelectorAll("a, span, p, th, td, .row, h2");
-      texts.forEach((text) => {
-        text.style.color = textColor;
-      });
-      const actives = document.querySelectorAll(".selected-date, a.active");
-      if (actives[0]) {
-        actives.forEach((active) => {
-          active.style.color = bgColor;
-        });
-      }
-    };
-
-    const headerFix = () => {
-      const statTableHeaders = document.querySelectorAll(
-        ".sticky-header-cloned-wrapper"
-      );
-      const pageNavbar = document.querySelector(".navbar-item.secondary");
-
-      if (statTableHeaders) {
-        statTableHeaders.forEach((statTableHeader) => {
-          statTableHeader.style.top = `${pageNavbar.offsetHeight}px`;
-        });
-      }
-    };
-
-    changeBgColors();
-    changeTextColor();
-    headerFix();
-
-    document.addEventListener("scroll", () => {
-      changeBgColors();
-      changeTextColor();
-      headerFix();
-    });
+const deleteClass = () => {
+  const bgImportants = document.querySelectorAll(".bg-white, .bg-light");
+  bgImportants.forEach((bgImportant) => {
+    bgImportant.classList.remove("bg-white");
+    bgImportant.classList.remove("bg-light");
   });
+};
+
+const headerFix = () => {
+  const statTableHeaders = document.querySelectorAll(
+    ".sticky-header-cloned-wrapper"
+  );
+  const pageNavbar = document.querySelector(".navbar-item.secondary");
+
+  if (statTableHeaders) {
+    statTableHeaders.forEach((statTableHeader) => {
+      statTableHeader.style.top = `${pageNavbar.offsetHeight}px`;
+    });
+  }
+};
+
+deleteClass();
+headerFix();
+
+document.addEventListener("scroll", () => {
+  deleteClass();
+  headerFix();
 });
